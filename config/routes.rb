0ca1a3fab0e176ed7b43533ba62605f5a594b26f
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-06-05 12:41:20 +0000

    [Add]ユーザログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "public/sessions",
+    passwords: "public/passwords",
+    registrations: "public/registrations"
+  }
   
   scope module: :public do
     root "homes#top"
