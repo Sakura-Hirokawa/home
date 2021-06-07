e3675043a4cc52594020fea8166adf514cfcb385
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-06-07 02:21:19 +0000

    [add]ユーザマイページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  attachment :profile_image
+  
   has_many :lists, dependent: :destroy
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9aaccf0e6356c1e7bc2b2542932db4d09b2eb7f1cc7a2c6603e6b33af787f264662df78d713dc99bf8ec6b50385926fc3fd2a55e9cd5f5f5ef96575b8f7208d8'
